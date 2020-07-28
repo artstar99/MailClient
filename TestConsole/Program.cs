fb6904a18fbedb0e4f285010d09b835e762c1e4a
artstar99 <artstar99@gmail.com>
2020-07-28 15:05:33 +0300

    Написали почтовую программу в консоли. Тестовый вариант

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Security;
 
 namespace TestConsole
 {
@@ -10,6 +13,29 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            var from = new MailAddress("artstar99@gmail.com", "Kostya");
+            var to =new MailAddress("artcore.gen@gmail.com");
+            var m = new MailMessage(from, to) {Subject = "Тест", Body = "Тестовое письмо", IsBodyHtml = false};
+
+            var smtp = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential("artstar99@gmail.com", "")
+            };
+
+            try
+            {
+                smtp.Send(m);
+                Console.WriteLine("Письмо должно было быть отправлено");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Невозможно отправить письмо" + e.ToString());
+            }
+
+            Console.ReadKey();
         }
     }
 }
