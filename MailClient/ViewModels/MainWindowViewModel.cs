766918ef0c9501bc74763b08d2c3c2edb1c8693b
artstar99 <artstar99@gmail.com>
2020-07-28 19:50:12 +0300

    Созданы классы ViewModel, работа с интерфейсом

---
+++ MailClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MailClient.Annotations;
+
+namespace MailClient.ViewModels
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+        private double halfWindowSize;
+
+        public double HalfWindowSize
+        {
+            get => halfWindowSize;
+            set => halfWindowSize = value;
+        }
+
+      
+    }
+}
