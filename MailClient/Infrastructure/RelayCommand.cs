40bf68d87b1fa93afafe362e23a7a2736385c764
artstar99 <artstar99@gmail.com>
2020-07-29 13:46:10 +0300

    Главный класс комманды

---
+++ MailClient/Infrastructure/RelayCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailClient.Infrastructure
+{
+    class RelayCommand:BaseCommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public override bool CanExecute(object parameter)=> canExecute?.Invoke(parameter) ?? true;
+        public override void Execute(object parameter)=> execute(parameter);
+        
+
+        public RelayCommand(Action<object> Execute, Func<object,bool> CanExecute =null)
+        {
+            execute = Execute?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+    }
+}
