9ca9ddb049e36db7454a73ca16b61df343bdbcfe
artstar99 <artstar99@gmail.com>
2020-07-29 13:14:36 +0300

    Добавлены классы MailSender SmtpData

---
+++ MailClient/Data/SmtpData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailClient.Data
+{
+    public static class SmtpData
+    {
+        private static int gmailPort = 587;
+        private static string gmailHost = "smtp.gmail.com";
+
+        public static string GmailHost
+        {
+            get => gmailHost;
+            set => gmailHost = value;
+        }
+
+        public static int GmailPort
+        {
+            get => gmailPort;
+            set => gmailPort = value;
+        }
+    }
+}
---
+++ MailClient/Models/MailSender.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MailClient.Data;
+
+
+namespace MailClient.Models
+{
+    class MailSender
+    {
+        private MailMessage mail;
+        private SmtpClient smtp;
+
+        public MailMessage Mail { get => mail; set => mail = value; }
+
+        public SmtpClient Smtp { get => smtp; set => smtp = value; }
+
+        public void Send(string from, string to, string theme, string body)
+        {
+
+            mail = new MailMessage(from, to) { Subject = theme, Body = body, IsBodyHtml = false };
+
+            smtp = new SmtpClient(SmtpData.GmailHost, SmtpData.GmailPort)
+            {
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential("artstar99@gmail.com", "")
+            };
+
+            try
+            {
+                smtp.Send(mail);
+                MessageBox.Show("Письмо должно было быть отправлено");
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show("Невозможно отправить письмо" + e.ToString());
+            }
+        }
+
+        public void AddAttachments()
+        {
+        }
+        public void Send(string from, string to)
+        {
+        }
+
+        public MailSender()
+        {
+                
+        }
+
+    }
+}
